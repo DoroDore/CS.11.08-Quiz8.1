315ee7e3a307a89b219cc81a0bf82c6405c7c3dc
DoroDore <tlyz82@student.cis.eduhk>
2024-03-06 08:59:54 +0800

    I tried to get the challenge but ran outta time :(((

--- src/TodoList.java
+++ src/TodoList.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TodoList {
 
+    public static void main(String[] args) {
+
+    }
     private String owner;
     private ArrayList<Task> tasks;
 
@@ -86,14 +90,41 @@ public class TodoList {
      *
      * BONUS Challenge:  Sort your to-do list in descending order of urgency
      */
-    public String toString() {
-        /*String thing = "To-do List of " + owner;
+    /*public static ArrayList<Task> sorter(ArrayList<Task> tasks) {
+        int mostUrgent = 0;
+        ArrayList<Task> thing = new ArrayList<Task>();
+        ArrayList<Task> temp = tasks;
+        while (!temp.isEmpty()) {
+            for (int i = 0; i < tasks.size(); i++) {
+                if (temp.get(i).getUrgency() > mostUrgent) {
+                    mostUrgent = temp.get(i).getUrgency();
+                }
+            }
+            thing.add(temp.get(mostUrgent));
+            temp.remove(mostUrgent);
+        }
+        return thing;
+    }
+    public String toStringSorted() {
+        ArrayList<Task> temp = sorter(tasks);
+        String thing = "To-do List of " + owner;
         for (int i = 0; i < tasks.size(); i++) {
-            thing += ("\n" + tasks.get(i).toString());
+            thing += ("\n" + toText(i));
         }
         System.out.println(thing);
-        return thing; */
-        return"";
+        return thing;    } */
+    public String toString() {
+        String thing = "To-do List of " + owner;
+        for (int i = 0; i < tasks.size(); i++) {
+            thing += ("\n" + toText(i));
+        }
+        System.out.println(thing);
+        return thing;
     }
+    public String toText(int index) {
+        return (tasks.get(index).getName() + "\t" + tasks.get(index).getUrgency());
+    }
+    /**This is my method that I'll use to sort the results*/
+
 
 }
